70011d8628b75539fbc13a0e59a3c1cc2471c60e
nickolas543 <prevencione2@hotmail.com>
2026-04-22 00:03:38 -0500

    Actualización en pom.xml y  realizo la tabla entitie Usuario.java

--- src/main/java/pe/upc/smartpoolguardian/entities/Usuario.java
+++ src/main/java/pe/upc/smartpoolguardian/entities/Usuario.java
@@ -1,4 +1,29 @@
 package pe.upc.smartpoolguardian.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int usuario_id;
+
+    @Column(name = "nombre_usuario", length = 50, nullable = false)
+    private String nombre_usuario;
+    @Column(name = "contraseña", length = 100, nullable = false)
+    private String contraseña;
+    @Column(name = "email", length = 100, nullable = false)
+    private String email;
+    @Column(name = "numero_celular", length = 11, nullable = false)
+    private String numero_celular;
+
 }
