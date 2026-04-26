c94bf6ec0fd0bcf7c31231fe0909d1cd34817504
nickolas543 <prevencione2@hotmail.com>
2026-04-26 16:15:59 -0500

    Se reemplazo por lo creado de juandie

--- src/main/java/pe/upc/smartpoolguardian/servicesinterfaces/IPiscinaService.java
+++ src/main/java/pe/upc/smartpoolguardian/servicesinterfaces/IPiscinaService.java
@@ -1,5 +1,6 @@
 package pe.upc.smartpoolguardian.servicesinterfaces;
 
+<<<<<<< HEAD
 import org.springframework.data.repository.query.Param;
 import pe.upc.smartpoolguardian.schema.dtos.PiscinasPorUsuarioDTO;
 import pe.upc.smartpoolguardian.entities.Piscina;
@@ -11,4 +12,17 @@ public interface IPiscinaService {
     public List<PiscinasPorUsuarioDTO> mostrarPiscinasPorUsuario(int id);
     public Piscina actualizarPiscina(Piscina piscina);
     public void eliminarPiscina(Piscina piscina);
+=======
+import pe.upc.smartpoolguardian.entities.Piscina;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IPiscinaService {
+    public void delete(int id);
+    public Piscina insert(Piscina p);
+    public Optional<Piscina> listId(int id);
+    List<Piscina>listarPiscinasPorUsuarios(int idUsuario);
+    public void update(Piscina p);
+>>>>>>> efc206b (Se crearon los endpoints de piscinas : buscar Piscinas por id, eliminar de piscinas, actualizar piscinas,listar piscinas por idusuario,registrar piscinas)
 }
